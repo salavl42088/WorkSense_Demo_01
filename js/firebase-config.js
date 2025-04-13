1ea6a140b19ebaa2e8d6a3dedfcd7ce65336fd47
salavl42088 <lindseyshane50@gmail.com>
2025-04-13 06:45:29 -0500

    load all pages
    upload test of registger pages

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+<script>
+  const firebaseConfig = 
+
+    apiKey: "AIzaSyCzYltvIlyd9a7c1zv_nHbi_sG06cg9l9Yo",
+    authDomain: "worksense-822c4.firebaseapp.com",
+    projectId: "worksense-822c4",
+    storageBucket: "worksense-822c4.appspot.com",
+    messagingSenderId: "447733676713",
+    appId "1:447733676713:web:12486d57ef923fc15247b8"
+  ;
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+</script>
